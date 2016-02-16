852a21b3272903fbb0526f7ed921f081f0bd3c32
Jason <Jcory.ng@gmail.com>
2016-02-16 17:47:32 -0500

    changing CBs to named cbs

--- app.js
+++ app.js
@@ -27,31 +27,47 @@ app.get('/whiskeys', function(req, res){
 //maybe make two endpoints, so users can choose how to write their code and pass back data?
 //one way would be to have a search bar that will take in a name. Still need to use params instead of body because the /whiskeys endpoint already exists
 
-app.get('/whiskeys/:id', function(req, res){
+var single = function(req, res){
 	var wID = req.params.id;
+
+	db.get("SELECT * FROM whiskey WHERE id=?", wID, function(err, row){
+		if (err){
+			throw err
+		} else {
+			res.send(row)
+		}
+	})
+}
+
+app.get('/whiskeys/:id', single)
+
+
+	// function(req, res){
+	
 	// var wID = req.body.name;
 
-	db.get("SELECT * FROM whiskey WHERE id = ?", wID, function(err, row){
-		if (err){
-			throw err
-		} else {
-			res.send(row)
-		}
-	})
-})
+	// db.get("SELECT * FROM whiskey WHERE id = ?", wID, function(err, row){
+	// 	if (err){
+	// 		throw err
+	// 	} else {
+	// 		res.send(row)
+	// 	}
+	// })
+// })
 
-app.get('/whiskeys/edit/:id', function(req,res){
-	var wID = req.params.id;
+app.get('/whiskeys/:id/edit', single);
+// 	function(req,res){
+// 	var wID = req.params.id;
 
-	db.get("SELECT * FROM whiskey WHERE id = ?", wID, function(err, row){
-		if (err){
-			throw err
-		} else {
-			res.send(row)
-		}
-	})
-	console.log("GET Whiskeys id edit")
-})
+// 	db.get("SELECT * FROM whiskey WHERE id = ?", wID, function(err, row){
+// 		if (err){
+// 			throw err
+// 		} else {
+// 			res.send(row)
+// 		}
+// 	})
+// 	console.log("GET Whiskeys id edit")
+// })
 
 app.post('/whiskeys/create', function(req,res){
 	console.log("WE ARE IN POST CREATE")
