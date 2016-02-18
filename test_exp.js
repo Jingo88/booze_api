54bee6282466f8035a196773568517f1bd0f1fa6
Jason <Jcory.ng@gmail.com>
2016-02-18 15:07:58 -0500

    made bad word replacer function

---
+++ test_exp.js
@@ -0,0 +1,5 @@
+var mexp = require("./wording.js")
+
+var name = "fuck jason he is a fucking moron fuck fuck"
+
+console.log(mexp.replace_bad(name))
--- wording.js
+++ wording.js
@@ -10,12 +10,20 @@ mexp.titleCase = function(x){
 	return str
 }
 
+mexp.replace_bad = function(x){
+	var str = x.split(" ")
+	for (var i = 0; i<str.length; i++){
+		var copy = str[i].substring(1).toLowerCase();
+		str[i] = str[i][0].toUpperCase() + copy;
+		if (badWords.indexOf(str[i]) >= 0){
+			str[i] = "puppies"
+		}
+	}
+	str = str.join(" ");
+	return str
+}
+
+
 var badWords = [
-
+	"Fuck"
 ];
-
-
-
-
-
-
