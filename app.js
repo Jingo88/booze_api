47e17337b32a021fc152d7dc47921950a419e575
Jason <Jcory.ng@gmail.com>
2016-11-21 18:53:21 -0500

    working?

--- app.js
+++ app.js
@@ -2,12 +2,20 @@
 var express = require('express');											//express
 var app = express()																		//express
 app.set('port', (process.env.PORT || 8080));					//set your port
+
 var bodyParser = require('body-parser');							//middleware
 app.use(bodyParser.json());														//middleware
 app.use(bodyParser.urlencoded({ extended: true })); 	//middleware
-var sqlite3 = require('sqlite3').verbose();						//database
-var db = new sqlite3.Database('whiskey.db');					//database
-var mexp = require("./wording.js") 										//import the wording.js file
+
+var whiskey_routes = require('./routes/whiskeys');
+  
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use('/', whiskey_routes);
 
 //get rid of favicon bullshit
 app.get('/favicon.ico', function(req, res){
@@ -15,141 +23,10 @@ app.get('/favicon.ico', function(req, res){
 	res.end();
 });
 
-//HOME ROUTE
-app.get('/', function(req, res){
-	res.redirect('/whiskeys')
-})
-app.get('/whiskeys', function(req, res){
-	db.all("SELECT * FROM whiskey", function(err, row){
-		if (err){
-			res.send("Hmmm there's something wrong with the root route? Server shutting down")
-			throw err
-		}else{
-			res.jsonp(row)		
-		}
-	});
-});
-
-//route to let a user search by name
-app.get('/whiskeys/search/:name', function(req,res){
-
-	var param= req.params.name;
-	var wName = mexp.titleCase(param);
-
-	db.all("SELECT * FROM whiskey WHERE name LIKE ?", "%"+wName+"%", function(err, row){
-		if(err){
-			res.send("There was an error searching for that name")
-		} else {
-			if (row.length < 1){
-				res.send("Sorry that whiskey does not exist");
-			} else {
-				res.jsonp(row)
-			}
-		}
-	})
-});
-
-//This is the named callback for the GET requests for a single whiskey
-var singleID = function(req, res){
-	var wID = req.params.id;
-
-	if (isNaN(wID)){
-		res.send("Sorry that is not a valid ID. Please enter a integer")
-	} else {
-		db.get("SELECT * FROM whiskey WHERE id=?", wID, function(err, row){
-			if (err){
-				res.send("There seems to be an error by searching for that ID")
-			} else {
-				if (row === undefined){
-					res.send("There is no whiskey with that ID")
-				} else{
-					res.jsonp(row)	
-				}
-			}
-		})
-	}
-}
-
-//GET requests for a single whiskey
-app.get('/whiskeys/:id', singleID);
-app.get('/whiskeys/:id/update', singleID);
-
-
-//POST request for creating a whiskey
-app.post('/whiskeys/create', function(req,res){
-	var rName = req.body.name;
-	var rType = req.body.type;
-	var rPrice = req.body.price;
-
-	var name = mexp.titleCase(rName);
-	var type = mexp.titleCase(rType);
-	var price = mexp.priceChecker(rPrice);
-
-	if (isNaN(price) === true){
-		res.send("You entered an invalid price, please make sure to enter an integer");
-	} else {
-		db.run("INSERT INTO whiskey (name, type, price) VALUES (?, ?, ?)", name, type, price, function(err){
-			if(err){
-				// throw err;
-				res.send(name + " already exists")
-			} else {
-				res.send("You have created "+ name + " in our database")
-			}
-		})
-	}
-});
-
-//POST requests for updating whiskey
-app.put('/whiskeys/:id/update', function(req,res){
-	var wID = req.params.id;
-	var rName = req.body.name;
-	var rType = req.body.type;
-	var rPrice = req.body.price;
-
-	var name = mexp.titleCase(rName);
-	var type = mexp.titleCase(rType);
-	var price = mexp.priceChecker(rPrice);
-
-	if (isNaN(price)===true){
-		res.send("You entered an invalid price, please make sure to enter an integer");
-	} else {
-		db.run("UPDATE whiskey SET name=?, type=?, price=? WHERE id=?",name, type, price, wID, function(err){
-			if(err){
-				throw err
-			} else {
-				res.send("We have updated the database with " + name)
-			}
-		})
-	}
-});
-
-//DELETE Whiskey
-app.delete('/whiskeys/:id/delete', function(req, res){
-	var delID = req.params.id;
-
-	if (isNaN(delID)){
-		res.send("You have entered an invalid id, please enter an integer")
-	} else {
-		db.run("DELETE FROM whiskey WHERE id=?", delID, function(err, row){
-			if (err){
-				res.send("There is an error with your request")
-			} else {
-				if (row === undefined){
-					res.send("There is no whiskey with that ID")
-				} else{
-					res.send("Whiskey has been deleted from the database")
-				}
-			}
-		})		
-	}
-});
-
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
 
 //check out sqlite3 wrapper commands ---- .run / .all / .get
 
@@ -160,13 +37,6 @@ app.listen(app.get('port'), function() {
 // PUT /whiskeys/:id/update 						Update One Whiskey
 // POST /whiskeys/create 								Create New Whiskey
 // DELETE /whiskeys/:id/delete 					Delete One Whiskey
-
-
-
-// TODO
-
-// make something to prevent a billion curl posts
-
 
 
 
---
+++ routes/whiskeys.js
@@ -0,0 +1,141 @@
+var express = require('express');
+
+var sqlite3 = require('sqlite3').verbose();						//database
+var db = new sqlite3.Database('whiskey.db');					//database
+var mexp = require("../wording.js") 										//import the wording.js file
+
+var router = express.Router();
+
+router.route('/')
+	.get(function(req,res){
+		res.redirect('/whiskeys')
+	})
+
+router.route('/whiskeys')
+	.get(function(req,res){
+		db.all("SELECT * FROM whiskey", function(err, row){
+			if (err){
+				res.send("Hmmm there's something wrong with the root route? Server shutting down")
+				throw err
+			}else{
+				res.jsonp(row)		
+			}
+		});
+	})
+
+// //route to let a user search by name
+// app.get('/whiskeys/search/:name', function(req,res){
+
+// 	var param= req.params.name;
+// 	var wName = mexp.titleCase(param);
+
+// 	db.all("SELECT * FROM whiskey WHERE name LIKE ?", "%"+wName+"%", function(err, row){
+// 		if(err){
+// 			res.send("There was an error searching for that name")
+// 		} else {
+// 			if (row.length < 1){
+// 				res.send("Sorry that whiskey does not exist");
+// 			} else {
+// 				res.jsonp(row)
+// 			}
+// 		}
+// 	})
+// });
+
+// //This is the named callback for the GET requests for a single whiskey
+// var singleID = function(req, res){
+// 	var wID = req.params.id;
+
+// 	if (isNaN(wID)){
+// 		res.send("Sorry that is not a valid ID. Please enter a integer")
+// 	} else {
+// 		db.get("SELECT * FROM whiskey WHERE id=?", wID, function(err, row){
+// 			if (err){
+// 				res.send("There seems to be an error by searching for that ID")
+// 			} else {
+// 				if (row === undefined){
+// 					res.send("There is no whiskey with that ID")
+// 				} else{
+// 					res.jsonp(row)	
+// 				}
+// 			}
+// 		})
+// 	}
+// }
+
+// //GET requests for a single whiskey
+// app.get('/whiskeys/:id', singleID);
+// app.get('/whiskeys/:id/update', singleID);
+
+
+// //POST request for creating a whiskey
+// app.post('/whiskeys/create', function(req,res){
+// 	var rName = req.body.name;
+// 	var rType = req.body.type;
+// 	var rPrice = req.body.price;
+
+// 	var name = mexp.titleCase(rName);
+// 	var type = mexp.titleCase(rType);
+// 	var price = mexp.priceChecker(rPrice);
+
+// 	if (isNaN(price) === true){
+// 		res.send("You entered an invalid price, please make sure to enter an integer");
+// 	} else {
+// 		db.run("INSERT INTO whiskey (name, type, price) VALUES (?, ?, ?)", name, type, price, function(err){
+// 			if(err){
+// 				// throw err;
+// 				res.send(name + " already exists")
+// 			} else {
+// 				res.send("You have created "+ name + " in our database")
+// 			}
+// 		})
+// 	}
+// });
+
+// //POST requests for updating whiskey
+// app.put('/whiskeys/:id/update', function(req,res){
+// 	var wID = req.params.id;
+// 	var rName = req.body.name;
+// 	var rType = req.body.type;
+// 	var rPrice = req.body.price;
+
+// 	var name = mexp.titleCase(rName);
+// 	var type = mexp.titleCase(rType);
+// 	var price = mexp.priceChecker(rPrice);
+
+// 	if (isNaN(price)===true){
+// 		res.send("You entered an invalid price, please make sure to enter an integer");
+// 	} else {
+// 		db.run("UPDATE whiskey SET name=?, type=?, price=? WHERE id=?",name, type, price, wID, function(err){
+// 			if(err){
+// 				throw err
+// 			} else {
+// 				res.send("We have updated the database with " + name)
+// 			}
+// 		})
+// 	}
+// });
+
+// //DELETE Whiskey
+// app.delete('/whiskeys/:id/delete', function(req, res){
+// 	var delID = req.params.id;
+
+// 	if (isNaN(delID)){
+// 		res.send("You have entered an invalid id, please enter an integer")
+// 	} else {
+// 		db.run("DELETE FROM whiskey WHERE id=?", delID, function(err, row){
+// 			if (err){
+// 				res.send("There is an error with your request")
+// 			} else {
+// 				if (row === undefined){
+// 					res.send("There is no whiskey with that ID")
+// 				} else{
+// 					res.send("Whiskey has been deleted from the database")
+// 				}
+// 			}
+// 		})		
+// 	}
+// });
+
+
+module.exports = router;
