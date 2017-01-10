67fe817aaae1a123f641c5387e11317494157400
Jason <Jcory.ng@gmail.com>
2017-01-10 17:33:05 -0500

    cors

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var whiskey_routes = require('./routes/whiskeys');
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, HEAD");
   next();
 });
 
